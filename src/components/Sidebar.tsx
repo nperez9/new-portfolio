144c28764beeefec5c909488e462ef46b802fc14
Nicolas Agustin Perez <nicolasperez95.a@gmail.com>
2024-09-10 00:38:19 -0300

    feat: fade to black

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,17 +1,17 @@
-"use client";
-import { navlinks } from "@/constants/navlinks";
-import { Navlink } from "@/types/navlink";
-import Image from "next/image";
-import Link from "next/link";
-import { useRouter, usePathname } from "next/navigation";
-import React, { useState } from "react";
-import { twMerge } from "tailwind-merge";
-import { Heading } from "./Heading";
-import { socials } from "@/constants/socials";
-import { Badge } from "./Badge";
-import { AnimatePresence, motion } from "framer-motion";
-import { IconLayoutSidebarRightCollapse } from "@tabler/icons-react";
-import { isMobile } from "@/lib/utils";
+'use client';
+import { navlinks } from '@/constants/navlinks';
+import { Navlink } from '@/types/navlink';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter, usePathname } from 'next/navigation';
+import React, { useState } from 'react';
+import { twMerge } from 'tailwind-merge';
+import { Heading } from './Heading';
+import { socials } from '@/constants/socials';
+import { Badge } from './Badge';
+import { AnimatePresence, motion } from 'framer-motion';
+import { IconLayoutSidebarRightCollapse } from '@tabler/icons-react';
+import { isMobile } from '@/lib/utils';
 
 export const Sidebar = () => {
   const [open, setOpen] = useState(isMobile() ? false : true);
@@ -23,9 +23,9 @@ export const Sidebar = () => {
           <motion.div
             initial={{ x: -200 }}
             animate={{ x: 0 }}
-            transition={{ duration: 0.2, ease: "linear" }}
+            transition={{ duration: 0.2, ease: 'linear' }}
             exit={{ x: -200 }}
-            className="px-6  z-[100] py-10 bg-neutral-100 max-w-[14rem] lg:w-fit  fixed lg:relative  h-screen left-0 flex flex-col justify-between"
+            className="px-6  z-[100] py-10 bg-stone-800 text-stone-200 max-w-[14rem] lg:w-fit  fixed lg:relative  h-screen left-0 flex flex-col justify-between"
           >
             <div className="flex-1 overflow-auto">
               <SidebarHeader />
@@ -47,11 +47,7 @@ export const Sidebar = () => {
   );
 };
 
-export const Navigation = ({
-  setOpen,
-}: {
-  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
-}) => {
+export const Navigation = ({ setOpen }: { setOpen: React.Dispatch<React.SetStateAction<boolean>> }) => {
   const pathname = usePathname();
 
   const isActive = (href: string) => pathname === href;
@@ -64,16 +60,11 @@ export const Navigation = ({
           href={link.href}
           onClick={() => isMobile() && setOpen(false)}
           className={twMerge(
-            "text-secondary hover:text-primary transition duration-200 flex items-center space-x-2 py-2 px-2 rounded-md text-sm",
-            isActive(link.href) && "bg-white shadow-lg text-primary"
+            'text-secondary hover:text-primary transition duration-200 flex items-center space-x-2 py-2 px-2 rounded-md text-sm',
+            isActive(link.href) && 'bg-white shadow-lg text-primary',
           )}
         >
-          <link.icon
-            className={twMerge(
-              "h-4 w-4 flex-shrink-0",
-              isActive(link.href) && "text-sky-500"
-            )}
-          />
+          <link.icon className={twMerge('h-4 w-4 flex-shrink-0', isActive(link.href) && 'text-sky-500')} />
           <span>{link.label}</span>
         </Link>
       ))}
@@ -86,15 +77,10 @@ export const Navigation = ({
           key={link.href}
           href={link.href}
           className={twMerge(
-            "text-secondary hover:text-primary transition duration-200 flex items-center space-x-2 py-2 px-2 rounded-md text-sm"
+            'text-secondary hover:text-primary transition duration-200 flex items-center space-x-2 py-2 px-2 rounded-md text-sm',
           )}
         >
-          <link.icon
-            className={twMerge(
-              "h-4 w-4 flex-shrink-0",
-              isActive(link.href) && "text-sky-500"
-            )}
-          />
+          <link.icon className={twMerge('h-4 w-4 flex-shrink-0', isActive(link.href) && 'text-sky-500')} />
           <span>{link.label}</span>
         </Link>
       ))}
@@ -113,7 +99,7 @@ const SidebarHeader = () => {
         className="object-cover object-top rounded-full flex-shrink-0"
       />
       <div className="flex text-sm flex-col">
-        <p className="font-bold text-primary">John Doe</p>
+        <p className="font-bold text-primary">Nicolas Perez</p>
         <p className="font-light text-secondary">Developer</p>
       </div>
     </div>
