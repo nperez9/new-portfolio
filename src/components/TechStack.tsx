f9052676b787462485c49826e776373880c908d1
HDPEREZNA <Nicolas.Perez@harley-davidson.com>
2024-12-22 21:40:13 -0300

    feat: added stuff

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,62 +1,43 @@
-import Image from "next/image";
-import React from "react";
-import { Heading } from "./Heading";
-import { twMerge } from "tailwind-merge";
+import Image from 'next/image';
+import React from 'react';
+
+import { Heading } from './Heading';
+import { twMerge } from 'tailwind-merge';
 
 export const TechStack = () => {
   const stack = [
     {
-      title: "Next.js",
-      src: "/images/logos/next.png",
+      title: 'Next.js',
+      src: '/images/logos/next.png',
 
-      className: "h-10 w-14",
+      className: 'h-10 w-14',
     },
     {
-      title: "AWS",
-      src: "/images/logos/aws.webp",
+      title: 'Node',
+      src: '/images/logos/node.png',
 
-      className: "h-10 w-10",
+      className: 'h-10 w-12',
     },
     {
-      title: "Figma",
-      src: "/images/logos/figma.png",
+      title: 'Tailwind',
+      src: '/images/logos/tailwind.png',
 
-      className: "h-10 w-8",
+      className: 'h-10 w-24',
     },
     {
-      title: "Framer Motion",
-      src: "/images/logos/framer.webp",
+      title: 'Vercel',
+      src: '/images/logos/vercel.png',
 
-      className: "h-10 w-10",
-    },
-    {
-      title: "Node",
-      src: "/images/logos/node.png",
-
-      className: "h-10 w-12",
-    },
-    {
-      title: "Tailwind",
-      src: "/images/logos/tailwind.png",
-
-      className: "h-10 w-24",
-    },
-    {
-      title: "Vercel",
-      src: "/images/logos/vercel.png",
-
-      className: "h-10 w-24",
+      className: 'h-10 w-24',
     },
   ];
+
   return (
-    <div>
-      <Heading
-        as="h2"
-        className="font-black text-lg md:text-lg lg:text-lg mt-20 mb-4"
-      >
+    <div className="mt-">
+      <Heading as="h2" className="font-black text-lg md:text-lg lg:text-lg mb-4">
         Tech Stack
       </Heading>
-      <div className="flex flex-wrap">
+      <div className="flex flex-wrap bg-white p-4 justify-between gap-1 rounded">
         {stack.map((item) => (
           <Image
             src={item.src}
@@ -64,7 +45,7 @@ export const TechStack = () => {
             width={`200`}
             height={`200`}
             alt={item.title}
-            className={twMerge("object-contain mr-4 mb-4", item.className)}
+            className={twMerge('object-contain', item.className)}
           />
         ))}
       </div>
